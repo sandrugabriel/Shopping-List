33fd2ab7d8b4024d47f023d194ba51fe491af6cd
Sandru Gabriel <gabi200611@yahoo.com>
2023-03-11 14:11:51 +0200

    done

--- Shopping-List/Home.Designer.cs
+++ Shopping-List/Home.Designer.cs
@@ -81,6 +81,7 @@
             this.button3.TabIndex = 0;
             this.button3.Text = "Home";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label2
